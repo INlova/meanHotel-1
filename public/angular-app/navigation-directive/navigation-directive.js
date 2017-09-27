ceb6ee72d964bb838ab22d5e74a53f29ad764485
zjaouad <zzjaouad@gmail.com>
2017-09-26 22:11:09 -0400

    deleted duplicate folder

--- public/angular-app/navigation-directive/navigation-directive.js
+++
@@ -1,8 +0,0 @@
-angular.module('meanhotel').directive('headNavigation', headNavigation);
-
-function headNavigation() {
-  return {
-    restrict: 'E',
-    templateUrl: 'angular-app/navigation-directive/navigation-directive.html'
-  };
-}
